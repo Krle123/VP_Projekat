0599bf934b7ccc1f00ef92fbf976fb6d3ca1bbdd
Krle123 <nesakrstonosic@gmail.com>
2025-09-27 17:37:54 +0200

    Add project files.

---
+++ ClassLibrary1/ISessionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    [ServiceContract]
+    public interface ISessionService
+    {
+        [OperationContract]
+        void StartSession();
+        [OperationContract]
+        void PushSample();
+        [OperationContract]
+        void EndSession();
+    }
+}
---
+++ ClassLibrary1/MotorSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    [DataContract]
+    public class MotorSample
+    {
+        private double i_q;
+        private double i_d;
+        private double coolant;
+        private int profile_id;
+        private double ambient;
+        private double torque;
+
+        [DataMember]
+        public double I_q { get { return i_q; } set { i_q = value; } }
+        [DataMember]
+        public double I_d { get { return i_d; } set { i_d = value; } }
+        [DataMember]
+        public double Coolant { get { return coolant; } set { coolant = value; } }
+        [DataMember]
+        public double Torque { get { return torque; } set { torque = value; } }
+        [DataMember]
+        public int ProfileId { get { return profile_id; } set { profile_id = value; } }
+        [DataMember]
+        public double Ambient { get { return ambient; } set { ambient = value; } }
+    }
+}
